d9743689d629cd51de3a5651732f5515a8484623
Paolo Invernizzi <paolo.invernizzi@suse.com>
2025-11-13 15:00:00 +0100

    Specify Azure provider source and version

--- backend_modules/azure/host/versions.tf
+++ backend_modules/azure/host/versions.tf
@@ -1,7 +1,13 @@
 terraform {
   required_version = ">= 1.6.0"
   required_providers {
-    null = ">= 2.1.0"
-    azurerm = "~> 2.40"
+    null = {
+      source  = "hashicorp/null"
+      version = ">= 2.1.0"
+    }
+    azurerm = {
+      source = "hashicorp/azurerm"
+      version = "~> 2.40"
+    }
   }
 }
