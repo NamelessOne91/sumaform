84dea37f104c7b6a9715a88d7b68b56b4c40db0d
Paolo Invernizzi <paolo.invernizzi@suse.com>
2025-12-04 11:54:42 +0100

    Add AWS Tumbleweed AMI

--- backend_modules/aws/base/main.tf
+++ backend_modules/aws/base/main.tf
@@ -68,6 +68,7 @@ locals {
     key_file = local.key_file
     iam_instance_profile = length(aws_iam_instance_profile.metering_full_access_instance_profile) > 0 ? aws_iam_instance_profile.metering_full_access_instance_profile[0].name : null
     ami_info = {
+      tumbleweedo                     = { ami = "ami-0ad3097969cc7f4c8" }, // deprecated image by "Innovators for openSUSE"                           }
       opensuse156o                    = { ami = data.aws_ami.opensuse156o.image_id },
       sles15sp4o                      = { ami = data.aws_ami.sles15sp4o.image_id },
       sles15sp5o                      = { ami = data.aws_ami.sles15sp5o.image_id },
