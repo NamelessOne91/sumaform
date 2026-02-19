35652e766f73c29bee221fad5ef399607ae7bf20
Paolo Invernizzi <paolo.invernizzi@suse.com>
2026-02-19 15:50:55 +0100

    Add variables for Server and Proxy custom additional repos

--- modules/build_validation/main.tf
+++ modules/build_validation/main.tf
@@ -99,8 +99,7 @@ module "server" {
   from_email                     = "root@suse.de"
   accept_all_ssl_protocols       = true
 
-  //server_additional_repos
-
+  additional_repos               = var.server_additional_repos
 }
 
 module "server_containerized" {
@@ -142,8 +141,7 @@ module "server_containerized" {
   from_email                     = "root@suse.de"
   provision                      = true
 
-  //server_additional_repos
-
+  additional_repos               = var.server_additional_repos
 }
 
 module "proxy" {
@@ -168,8 +166,7 @@ module "proxy" {
   use_os_released_updates   = true
   ssh_key_path              = "./salt/controller/id_ed25519.pub"
 
-  //proxy_additional_repos
-
+  additional_repos          = var.proxy_additional_repos
 }
 
 module "proxy_containerized" {
@@ -191,8 +188,7 @@ module "proxy_containerized" {
   ssh_key_path         = "./salt/controller/id_ed25519.pub"
   provision            = true
 
-  //proxy_additional_repos
-
+  additional_repos     = var.proxy_additional_repos
 }
 
 module "sles12sp5_minion" {
